fd2a0d8fae98f559acec979094dc38e5942f9cf1
AlbertGk <albertgk01@gmail.com>
2022-06-06 12:36:10 +0200

    template update

---
+++ src/components/FeedbackOptions.jsx
@@ -0,0 +1,8 @@
+
+export const FeedbackOptions = ({ options, onLeaveFeedback}) => {
+    return (
+          <button type="button" onClick={onLeaveFeedback}>
+            {options}
+          </button>
+    );
+}
